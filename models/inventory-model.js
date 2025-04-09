b072f90e21aa210532bb625deabc5156cc5ad4f3
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2025-04-09 05:40:48 +0000

    saving inv

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -3,7 +3,25 @@ const pool = require("../database/index.js");
  *  Get all classification data
  * ************************** */
 async function getClassifications() {
-    return await pool.query("SELECT * FROM public.classification ORDER BY classification_name");
+    const data =  await pool.query("SELECT * FROM public.classification ORDER BY classification_name");
+    return data;
 }
 
-module.exports = {getClassifications};
+/* ***************************
+ *  Get all inventory items and classification_name by classification_id
+ * ************************** */
+async function getInventoryByClassificationId(classification_id){
+    try {
+            const data = await pool.query(`SELECT * FROM public.inventory 
+                AS i JOin public.classification
+                AS c ON i.classification_id = c.classification_id
+                WHERE i.Classification_id = $1`, [classification_id])
+
+                return data.rows;
+    }
+    catch (error) {
+        console.error("getClassificationByID error" + error)
+    }
+}
+
+module.exports = {getClassifications, getInventoryByClassificationId};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const static = require("./routes/static");
 const expressEjsLayouts = require("express-ejs-layouts");
 const baseController =  require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 // Set up view engine
 app.set("view engine", "ejs");
@@ -12,9 +13,10 @@ app.set("layout", "./layouts/layout");
 
 // Routes
 app.use(static);
-/*app.get('/', (req, res) => {
-  res.render('index', { title: 'Home Page' });
-});*/
+app.use("/inv", inventoryRoute)
+
+
+
 app.get("/", baseController.buildHome);
 
 // Ensure PORT is set, or use 3000 as default
