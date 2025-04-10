ece37e17e04216cd6f57db8e33b7cc646285a006
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2025-04-10 10:39:50 +0000

    cs

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -8,7 +8,7 @@ async function getClassifications() {
 }
 
 /* ***************************
- *  Get all inventory items and classification_name by classification_id
+ *  Get all inventory items and classification_name by classification_id  
  * ************************** */
 async function getInventoryByClassificationId(classification_id){
     try {
