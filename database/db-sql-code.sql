0cb551d2ac9a15c2168d87b3c6688cb38e1dc68d
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2025-04-22 08:07:37 +0000

    cc

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -6,7 +6,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO database3405;
+    OWNER TO database3406;
 
 
 CREATE TABLE public.classification (
