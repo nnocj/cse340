dc97b2d1ad3e32a377d15f6befeed1f5a66e2fba
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2025-04-22 16:18:46 +0000

    change registration format

--- models/account-model.js
+++ models/account-model.js
@@ -4,9 +4,10 @@ const pool= require("../database/index");
 *   Register new account
 * *************************** */
 async function registerAccount(account_firstname, account_lastname, account_email, account_password) {
+    let account_type = "Client";
     try {
-        const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'client')  RETURNING *"; // first time reaching backend from the frontend view.
-        return await pool.query(sql, [account_firstname, account_lastname, account_email, account_password]);
+        const sql = "INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, $5)  RETURNING *"; // first time reaching backend from the frontend view.
+        return await pool.query(sql, [account_firstname, account_lastname, account_email, account_password, account_type]);
     }
     catch (error) {
         return error.message;
